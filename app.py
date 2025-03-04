de014b826ac48d9b24efa5fb2438379679a590ae
KingMgugga <104934731+KingMgugga@users.noreply.github.com>
2025-03-03 17:51:27 -0700

    Changed font back to Panton, updated app to remove ability for user to select which metrics are displayed

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ from highlight_text import fig_text, ax_text
 import matplotlib.patheffects as path_effects
 
 # Fonts
-font_light = "Kanit-Light.ttf"
-font_regular = "Kanit-Regular.ttf"
-font_bold = "Kanit-Bold.ttf"
+font_light = "Panton Light.otf"
+font_regular = "Panton Regular.otf"
+font_bold = "Panton Bold.otf"
 
 font_l = fm.FontProperties(fname=font_light)
 font_r = fm.FontProperties(fname=font_regular)
@@ -66,83 +66,75 @@ def run():
         metric_values = {metric: round(wallet_data[metric]) for metric in available_metrics.keys()}
         overall_score = round(wallet_data["overall_score"])  # Get overall_score separately
 
-        # Multiselect
-        selected_metrics = st.multiselect("Available Metrics", [available_metrics[m] for m in available_metrics])
+        # Use all metrics
+        selected_columns = list(available_metrics.keys())
+        values = [metric_values[m] for m in selected_columns]
+        params = [available_metrics[m] for m in selected_columns]
 
-        # Columns
-        selected_columns = [m for m in available_metrics if available_metrics[m] in selected_metrics]
+        # CMAP
+        norm = mcolors.Normalize(vmin=0, vmax=100)
+        start_color = '#2E2E2A'
+        end_color = st.color_picker("Select Color Scheme", value="#440981")
+        custom_colormap = mcolors.LinearSegmentedColormap.from_list("custom_gradient", [start_color, end_color], N=100)
+        slice_colors = [custom_colormap(norm(value)) for value in values]
 
-        if len(selected_columns) < 2:
-            st.warning("Select at least 2 metrics")
-        else:
-            # Values & lABELS
-            values = [metric_values[m] for m in selected_columns]
-            params = [available_metrics[m] for m in selected_columns]
+        # Pizza Chart
+        if st.button("Generate Chart"):
+            baker = PyPizza(
+                params=params,
+                background_color="#e9eae3",
+                straight_line_color="white",
+                straight_line_lw=1,
+                last_circle_color="white",
+                last_circle_lw=1,
+                other_circle_lw=0,
+                inner_circle_size=15
+            )
 
-            # CMAP
-            norm = mcolors.Normalize(vmin=0, vmax=100)
-            start_color = '#2E2E2A'
-            end_color = st.color_picker("Select Color Scheme", value="#440981")
-            custom_colormap = mcolors.LinearSegmentedColormap.from_list("custom_gradient", [start_color, end_color], N=100)
-            slice_colors = [custom_colormap(norm(value)) for value in values]
-
-            # Pizza Chart
-            if st.button("Generate Chart"):
-                baker = PyPizza(
-                    params=params,
-                    background_color="#e9eae3",
-                    straight_line_color="white",
-                    straight_line_lw=1,
-                    last_circle_color="white",
-                    last_circle_lw=1,
-                    other_circle_lw=0,
-                    inner_circle_size=15
+            fig, ax = baker.make_pizza(
+                values,
+                figsize=(9.5, 11),
+                blank_alpha=0.1,
+                param_location=111,
+                kwargs_slices=dict(edgecolor="white", zorder=2, linewidth=1, color=slice_colors),
+                kwargs_params=dict(color="black", fontsize=15, fontproperties=font_r, zorder=2, va="center"),
+                kwargs_values=dict(
+                    color="black", fontsize=13, fontproperties=font_b, zorder=3,
+                    bbox=dict(edgecolor="black", facecolor='#e9eae3', boxstyle="round,pad=0.2", lw=1.5)
                 )
+            )
 
-                fig, ax = baker.make_pizza(
-                    values,
-                    figsize=(9.5, 11),
-                    blank_alpha=0.1,
-                    param_location=111,
-                    kwargs_slices=dict(edgecolor="white", zorder=2, linewidth=1, color=slice_colors),
-                    kwargs_params=dict(color="black", fontsize=15, fontproperties=font_r, zorder=2, va="center"),
-                    kwargs_values=dict(
-                        color="black", fontsize=13, fontproperties=font_b, zorder=3,
-                        bbox=dict(edgecolor="black", facecolor='#e9eae3', boxstyle="round,pad=0.2", lw=1.5)
-                    )
-                )
+            ax.set_facecolor('#d9dad2')
 
-                ax.set_facecolor('#d9dad2')
+            # Titles
+            fig.text(
+                0.04, 0.960, title_input, size=30,
+                ha="left", fontproperties=font_b, color='black'
+            )
+        
+            fig.text(
+                0.04, 0.935,
+                suptitle_input,
+                size=15,
+                ha="left", fontproperties=font_r, color='#2E2E2A', alpha=0.8
+            )
 
-                # Titles
-                fig.text(
-                    0.04, 0.960, title_input, size=30,
-                    ha="left", fontproperties=font_b, color='black'
-                )
-            
-                fig.text(
-                    0.04, 0.935,
-                    suptitle_input,
-                    size=15,
-                    ha="left", fontproperties=font_r, color='#2E2E2A', alpha=0.8
-                )
+            # Text effect
+            text_effect = [path_effects.Stroke(linewidth=2.5, foreground=end_color), path_effects.Normal()]
 
-                # Text effect
-                text_effect = [path_effects.Stroke(linewidth=2.5, foreground=end_color), path_effects.Normal()]
+            # Overall_score
+            ax.text(
+                0, -15, f"{overall_score}", fontsize=32, alpha=1, fontproperties=font_b,
+                ha="center", va="center", color="black", bbox=dict(facecolor="none", edgecolor="none", boxstyle="circle,pad=0", zorder=-10),
+                path_effects=text_effect
+            )
 
-                # Overall_score
-                ax.text(
-                    0, -15, f"{overall_score}", fontsize=32, alpha=1, fontproperties=font_b,
-                    ha="center", va="center", color="black", bbox=dict(facecolor="none", edgecolor="none", boxstyle="circle,pad=0", zorder=-10),
-                    path_effects=text_effect
-                )
+            # Lines
+            fig.add_artist(plt.Line2D((0.0425, 0.9575), (0.900, 0.900), color=end_color, linewidth=8, alpha=1, transform=fig.transFigure))
+            fig.add_artist(plt.Line2D((0, 1), (0.88, 0.88), color="#e9eae3", linewidth=0.0001, alpha=0, zorder=-10, transform=fig.transFigure))
 
-                # Lines
-                fig.add_artist(plt.Line2D((0.0425, 0.9575), (0.900, 0.900), color=end_color, linewidth=8, alpha=1, transform=fig.transFigure))
-                fig.add_artist(plt.Line2D((0, 1), (0.88, 0.88), color="#e9eae3", linewidth=0.0001, alpha=0, zorder=-10, transform=fig.transFigure))
-
-                # Display
-                st.pyplot(fig)
+            # Display
+            st.pyplot(fig)
 
 # Run
 if __name__ == "__main__":
